e9596ef63a4f4eb3b88299c0ac7b1b5457ce7849
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2020-11-26 21:00:40 +0200

    Updates

---
+++ precache-manifest.a514fc595585fcbbaec4d9e12bc15fe9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e0dc08efc2026fdc23b0b398b2165c",
+    "url": "/space-x/index.html"
+  },
+  {
+    "revision": "bc0accecab013175af53",
+    "url": "/space-x/static/css/main.12a940d6.chunk.css"
+  },
+  {
+    "revision": "082b99b734d564e360cb",
+    "url": "/space-x/static/js/2.18aa7aea.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/space-x/static/js/2.18aa7aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc0accecab013175af53",
+    "url": "/space-x/static/js/main.b5ae56a3.chunk.js"
+  },
+  {
+    "revision": "9956faa1bb459329e951",
+    "url": "/space-x/static/js/runtime-main.2a0ba51f.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/space-x/static/media/logo.d5ab31ce.svg"
+  }
+]);
